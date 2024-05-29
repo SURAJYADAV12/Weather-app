c41121b14bc959059e621d9d1a2b974e390f20a4
SURAJYADAV12 <107552883+SURAJYADAV12@users.noreply.github.com>
2024-05-29 23:20:59 +0530

    https://github.com/SURAJYADAV12/Weather-app

---
+++ script.js
@@ -0,0 +1,11 @@
+var group1=document.getElementById("group1");
+var group2=document.getElementById("group2");
+var group3=document.getElementById("group3");
+var group4=document.getElementById("group4");
+var group5=document.getElementById("group5");
+var group6=document.getElementById("group6");
+var group7=document.getElementById("group7");
+var group8=document.getElementById("group8");
+var group9=document.getElementById("group9");
+ 
+ foreach()
